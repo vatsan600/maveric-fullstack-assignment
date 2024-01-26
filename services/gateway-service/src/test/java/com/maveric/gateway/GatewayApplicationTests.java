6afdf64694a7cb35430d4e9f7d0e7f4f7f0cd11d
vatsan <vatsan600@gmail.com>
2024-01-26 13:53:24 +0530

    Init commit - POM & application for all modules

---
+++ services/gateway-service/src/test/java/com/maveric/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maveric.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
