b0d1f79118221df2b6b3d27fd4148cbc9770a546
vatsan <vatsan600@gmail.com>
2024-01-26 17:30:08 +0530

    created eureka services

--- services/eureka-service/src/main/java/com/maveric/eureka/EurekaApplication.java
+++ services/eureka-service/src/main/java/com/maveric/eureka/EurekaApplication.java
@@ -1,9 +1,11 @@
-package com.maveric.account;
+package com.maveric.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
 	public static void main(String[] args) {
