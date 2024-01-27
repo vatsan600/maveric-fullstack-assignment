aa9c5e1cb17d1a574322fbec1d6a7d3e39f25443
vatsan <vatsan600@gmail.com>
2024-01-27 15:03:20 +0530

    implemented get, put & delete endpoints for  /customers/{customerId}/accounts/{accountId}

--- services/account-service/src/main/java/com/maveric/account/controller/AccountController.java
+++ services/account-service/src/main/java/com/maveric/account/controller/AccountController.java
@@ -6,12 +6,15 @@ import com.maveric.account.model.AccountWithoutBalance;
 import com.maveric.account.service.AccountService;
 import java.net.URI;
 import java.util.List;
+import java.util.Objects;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -51,52 +54,47 @@ public class AccountController {
         .body(createdAccount);
   }
 
-//  @GetMapping("/{userId}")
-//  public ResponseEntity<User> getUser(@PathVariable int userId) {
-//    logger.info("request for getting user using userId is received");
-//    logger.debug("The user id received from input is: {}", userId);
-//    User user = userService.getUserById(userId);
-//    if (Objects.nonNull(user)) {
-//      return ResponseEntity.ok().body(user);
-//    } else {
-//      return ResponseEntity.notFound().build();
-//    }
-//  }
-//
-//  @PutMapping("/{userId}")
-//  public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable int userId) {
-//    logger.info("request for updating user is received");
-//    logger.debug("The user data received from input is: {}", user);
-//    user.setUniqueId(userId);
-//    User updatedUser = userService.updateUser(user);
-//    if (Objects.nonNull(updatedUser)) {
-//      return ResponseEntity.ok().body(updatedUser);
-//    } else {
-//      return ResponseEntity.notFound().build();
-//    }
-//  }
-//
-//  @DeleteMapping("/{userId}")
-//  public ResponseEntity<User> deleteUser(@PathVariable int userId) {
-//    logger.info("request for deleting user is received");
-//    logger.debug("The unique Id user to be deleted is : {}", userId);
-//    if (userService.deleteUser(userId)) {
-//      return ResponseEntity.ok().build();
-//    } else {
-//      return ResponseEntity.notFound().build();
-//    }
-//  }
-//
-//  @GetMapping("/emails/{emailId}")
-//  public ResponseEntity<User> getUserByEmailId(@PathVariable String emailId) {
-//    logger.info("request for getting user by emailId is received");
-//    logger.debug("The emailId is: {}", emailId);
-//    User user = userService.getUserByEmailId(emailId);
-//    if (Objects.nonNull(user)) {
-//      return ResponseEntity.ok().body(user);
-//    } else {
-//      return ResponseEntity.notFound().build();
-//    }
-//  }
+  @GetMapping("/{customerId}/accounts/{accountId}")
+  public ResponseEntity<Account> getAccount(@PathVariable int customerId,
+      @PathVariable int accountId) {
+    logger.info("request for getting user using accountId is received");
+    logger.debug("The account id: {}, the customer id: {}", accountId, customerId);
+    Account account = accountService.getAccountById(customerId, accountId);
+    if (Objects.nonNull(account)) {
+      return ResponseEntity.ok().body(account);
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @PutMapping("/{customerId}/accounts/{accountId}")
+  public ResponseEntity<Account> updateAccount(@RequestBody AccountRequest accountRequest,
+      @PathVariable int customerId, @PathVariable int accountId) {
+    logger.info("request for updating user is received");
+    logger.debug(
+        "The user data received from input is: {}, the customer id: {}, the account id: {}",
+        accountRequest, customerId, accountId);
+    accountRequest.setCustomerId(customerId);
+    Account account = Account.of(accountRequest);
+    account.setUniqueId(accountId);
+    Account updatedAccount = accountService.updateAccount(customerId, account);
+    if (Objects.nonNull(updatedAccount)) {
+      return ResponseEntity.ok().body(updatedAccount);
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @DeleteMapping("/{customerId}/accounts/{accountId}")
+  public ResponseEntity<Account> deleteAccount(@PathVariable int customerId,
+      @PathVariable int accountId) {
+    logger.info("request for deleting user is received");
+    logger.debug("deleting account id:{} of customer id: {}", accountId,customerId);
+    if (accountService.deleteAccount(customerId,accountId)) {
+      return ResponseEntity.ok().build();
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
 
 }
--- services/account-service/src/main/java/com/maveric/account/service/AccountService.java
+++ services/account-service/src/main/java/com/maveric/account/service/AccountService.java
@@ -3,20 +3,20 @@ package com.maveric.account.service;
 import com.maveric.account.entity.Account;
 import com.maveric.account.model.AccountRequest;
 import com.maveric.account.model.AccountWithoutBalance;
+import com.maveric.account.model.User;
 import com.maveric.account.repository.AccountRepository;
 import com.maveric.account.restinterfaces.UserRestInterface;
+import feign.FeignException;
 import jakarta.ws.rs.NotFoundException;
-import java.sql.Date;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -37,9 +37,9 @@ public class AccountService {
       int customerId) {
     logger.info("getting users for page number: {} and page size: {}", pageNumber, pageNumber);
     Pageable page = PageRequest.of(pageNumber, pageSize);
-    return Optional.ofNullable(userRestInterface.getUser(customerId)).map(
+    return Optional.ofNullable(getCustomer(customerId)).map(
             user -> accountRepository.findAll(page).map(AccountWithoutBalance::of).stream().toList())
-        .orElseThrow(NotFoundException::new);
+        .orElse(null);
   }
 
   public Account saveAccount(Account account) {
@@ -55,41 +55,47 @@ public class AccountService {
   public Account saveAccountRequest(AccountRequest accountRequest) {
     logger.info("Creating a new account");
     logger.info("validating if the given customer id is present in the user table");
-    return Optional.ofNullable(userRestInterface.getUser(accountRequest.getCustomerId()))
+    return Optional.ofNullable(getCustomer(accountRequest.getCustomerId()))
         .map(user -> {
           Account account = Account.of(accountRequest);
           return saveAccount(account);
-        }).orElseThrow(NotFoundException::new);
+        }).orElse(null);
 
   }
 
-//  public User getUserById(int userId) {
-//    logger.info("getting user by id:{}", userId);
-//    return userRepository.findById(userId).orElse(null);
-//  }
-//
-//  public User updateUser(User user) {
-//    logger.info("getting user by id:{}", user);
-//    User updatedUser = getUserById(user.getUniqueId());
-//    if (Objects.nonNull(updatedUser)) {
-//      user.setUniqueId(updatedUser.getUniqueId());
-//      return saveUser(user);
-//    }
-//    return null;
-//  }
-//
-//  public boolean deleteUser(int userId) {
-//    logger.info("deleting user by id:{}", userId);
-//    if (Objects.nonNull(getUserById(userId))) {
-//      userRepository.deleteById(userId);
-//      return true;
-//    }
-//    return false;
-//  }
-//
-//  public User getUserByEmailId(String emailId) {
-//    logger.info("getting user by email id:{}", emailId);
-//    return userRepository.findByEmail(emailId);
-//  }
+  public Account getAccountById(int customerId, int accountId) {
+    logger.info("getting account by id:{}", accountId);
+    return Optional.ofNullable(getCustomer(customerId))
+        .map(user -> accountRepository.findById(accountId).orElse(null))
+        .orElse(null);
+  }
 
+  public Account updateAccount(int customerId, Account account) {
+    logger.info("updating account:{}", account);
+    Account updateAccount = getAccountById(customerId, account.getUniqueId());
+    if (Objects.nonNull(updateAccount)) {
+      account.setUniqueId(updateAccount.getUniqueId());
+      return saveAccount(account);
+    }
+    return null;
+  }
+
+
+  public boolean deleteAccount(int customerId, int accountId) {
+    logger.info("deleting account by id:{}", accountId);
+    if (Objects.nonNull(getAccountById(customerId, accountId))) {
+      accountRepository.deleteById(accountId);
+      return true;
+    }
+    return false;
+  }
+
+  private User getCustomer(int customerId) {
+    try {
+      return userRestInterface.getUser(customerId);
+    } catch (FeignException feignException) {
+      logger.error("exception while getting customer", feignException.getCause());
+      return null;
+    }
+  }
 }
