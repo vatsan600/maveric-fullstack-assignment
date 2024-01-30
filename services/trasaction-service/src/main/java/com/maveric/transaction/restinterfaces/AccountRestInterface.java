974278bbaeaa4d75264e25cf68c3f2d90fb8db57
vatsan <vatsan600@gmail.com>
2024-01-30 12:10:16 +0530

    proper routing through gateway & eureka, dockerfiles

--- services/trasaction-service/src/main/java/com/maveric/transaction/restinterfaces/AccountRestInterface.java
+++ services/trasaction-service/src/main/java/com/maveric/transaction/restinterfaces/AccountRestInterface.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "account-service", url = "localhost:8000/account-service")
+@FeignClient(value = "account-service")
 public interface AccountRestInterface {
 
   @GetMapping("/customers/{customerId}/accounts/{accountId}")
